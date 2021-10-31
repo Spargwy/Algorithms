ab22926e7c4b8e8d60289dd9b0d6a9f156d95917
Spargwy41 <dmitry.e@dot-tech.ru>
2021-10-31 14:30:17 +0600

    quickSort randomization

--- quick_sort/quicksort.go
+++ quick_sort/quicksort.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	array := []int{2, 8, 7, 1, 3, 5, 6, 1}
@@ -10,11 +13,17 @@ func main() {
 func quickSort(array []int, from, to int) []int {
 	var q int
 	if from < to {
-		array, q = partition(array, from, to)
+		array, q = randomizedPartition(array, from, to)
 		array = quickSort(array, from, q-1)
 		array = quickSort(array, q+1, to)
 	}
 	return array
+}
+
+func randomizedPartition(array []int, from, to int) ([]int, int) {
+	i := rand.Intn(to)
+	array[to] = array[i]
+	return partition(array, from, to)
 }
 
 //Изменяет массив так, что слева находятся
